85de16a5b13fb38eef7bcc82c7ad8b1511b91a92
ImoutoHeaven <noreply@imoutoheaven.org>
2026-01-23 18:54:24 +0800

    drop support for caas

--- caas/esm/esm.js
+++
@@ -1,3 +0,0 @@
-// Export the worker URL for use in glue.js
-// All actual PoW computation is performed in pow-worker.js
-export const workerUrl = new URL("./pow-worker.js", import.meta.url).toString();
